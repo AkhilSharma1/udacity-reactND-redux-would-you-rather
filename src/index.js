3667016c049bfdab7f16096e504b46e20cfa8bba
Akhil Sharma <akhil.sharma0910@gmail.com>
2018-08-31 16:45:51 -0400

    Add Redux root Provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import {Provider} from 'react-redux';
+import middleware from './middleware/index';
+import reducer from "./reducers";
+ 
+const store = createStore(reducer, middleware);
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
